0ed0fa7d77a2dbbac55cc194cbd85a627aecbdc3
Joseph Bock <Joseph_bock@hotmail.com>
2021-01-20 08:02:00 -0300

    Polishing CSS

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/views/index.html'));
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server Initialized");
 })
