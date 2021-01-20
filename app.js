0e5b3d616f075660126c2e2f173e6dc9aab372cb
Joseph Bock <Joseph_bock@hotmail.com>
2021-01-20 04:43:39 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/index.html'));
+})
+
+app.listen(3000, () => {
+    console.log("Server Initialized")
+})
